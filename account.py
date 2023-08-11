d77c1e3325df64d0d80bd3231198d1d417c8f808
Theclaw69 <108755048+Theclaw69@users.noreply.github.com>
2023-08-11 12:59:08 -0500

    tests added

--- account.py
+++ account.py
@@ -1,24 +1,29 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str):
+ 
         self.account_name = name
         self.account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+       
         if amount <= 0:
             return False
         else:
             self.account_balance += amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+
         if amount <= 0 or amount > self.account_balance:
             return False
         else:
             self.account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+
         return self.account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+
         return self.account_name
